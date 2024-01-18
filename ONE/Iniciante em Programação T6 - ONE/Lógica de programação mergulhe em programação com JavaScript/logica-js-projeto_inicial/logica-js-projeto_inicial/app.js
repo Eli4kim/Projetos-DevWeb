415f23a8e50be99250cd86fd23aebf1ad0c5a3ad
Eliakim A Araujo <eli4kim@gmail.com>
2024-01-18 08:12:53 -0300

    projetos One
    Adição de pasta e projetos de estudo Oracle Next Education

---
+++ ONE/Iniciante em Programação T6 - ONE/Lógica de programação mergulhe em programação com JavaScript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,20 @@
+// Gera um número aleatório entre 1 e 100
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+
+// Pede ao usuário para adivinhar o número secreto
+let tentativas = 0;
+let palpite = 0;
+
+while (palpite !== numeroSecreto) {
+  palpite = parseInt(prompt("Adivinhe o número secreto (entre 1 e 100):"));
+  tentativas++;
+
+  if (palpite < numeroSecreto) {
+    alert("Seu palpite é muito baixo. Tente novamente.");
+  } else if (palpite > numeroSecreto) {
+    alert("Seu palpite é muito alto. Tente novamente.");
+  }
+}
+
+// Parabeniza o usuário pela adivinhação correta
+alert(`Parabéns! Você adivinhou o número secreto em ${tentativas} tentativas.`);
